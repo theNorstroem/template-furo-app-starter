a97af4af8a17a55b41665ffcc655efbbb6c9e7b6
veith <veithz@gmail.com>
2019-11-17 19:05:11 +0100

    lint warnings

--- service-worker.js
+++ service-worker.js
@@ -8,4 +8,5 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
+/* eslint no-console: "off" */
 console.info('Service worker disabled for development, will be generated at build time.');
